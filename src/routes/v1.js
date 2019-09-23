de0556cacdcc503ec313f411b3b689f3d762d1be
JF <18373250@qq.com>
2019-09-23 16:50:32 +0800

    router modify

--- src/routes/v1.js
+++ src/routes/v1.js
@@ -4,29 +4,29 @@ const C = require('../controllers')
 /**
  * 路由前缀
  */
-const router = new Router({ prefix: '/v1' })
+const router = new Router({ prefix: '/v1/' })
 
-router.get('/elephant', C.elephant_ctrl.elephant)
+router.get('elephant', C.elephant_ctrl.elephant)
 
 /**
  * 新增用户信息
  */
-router.post('/user', C.user_ctrl.addUser)
+router.post('user', C.user_ctrl.addUser)
 
 /**
  * 获取用户信息
  */
-router.get('/user', A.auth, C.user_ctrl.getUser)
+router.get('user', A.auth, C.user_ctrl.getUser)
 
 /**
  * 更新用户信息
  */
-router.put('/user', A.auth, C.user_ctrl.updateUser)
+router.put('user', A.auth, C.user_ctrl.updateUser)
 
 /**
  * 删除用户信息
  */
-router.delete('/user', A.auth, C.user_ctrl.deleteUser)
+router.delete('user', A.auth, C.user_ctrl.deleteUser)
 
 
 
